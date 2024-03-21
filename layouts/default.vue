7d9b56770dd31d6db69159d9a6da3c277200c668
programmerkhadka <rituparnak@claritytech.io>
2024-03-21 22:19:44 +0530

    wip: Sidebar dropdown

--- layouts/default.vue
+++ layouts/default.vue
@@ -4,7 +4,10 @@
       class="w-full bg-white topbar min-h-[70px] border-b-2 py-2 px-4 grid grid-cols-2 items-center justify-between"
     >
       <div class="flex place-items-center">
-        <div class="border cursor-pointer" @click="isCollapse = !isCollapse">
+        <div
+          class="border rounded-md cursor-pointer hover:border-blue-600"
+          @click="isCollapse = !isCollapse"
+        >
           <Bars3Icon class="w-8 h-8" v-if="isCollapse" />
           <XMarkIcon class="w-8 h-8" v-else />
         </div>
@@ -18,10 +21,19 @@
     </div>
     <div class="relative flex">
       <div
-        class="min-h-screen bg-white border-r-2 sidebra transition-[width] duration-150 ease-in-out absolute left-0 xl:static"
+        class="min-h-screen bg-white border-r-2 sidebra transition-[width] duration-150 ease-in-out absolute left-0 xl:static overflow-hidden"
         :class="isCollapse ? 'w-0' : ' w-96'"
       >
-        <!-- THIS IS SIDE BAR -->
+        <ul class="flex flex-col">
+          <li class="relative flex items-center px-2 py-4 border">
+            <span>Menu 1</span>
+            <!-- <ChevronDownIcon class="absolute w-6 h-6 font-bold right-1" /> -->
+            <ul class="">
+              <li><span>Sub Menu 1</span></li>
+              <li><span>Sub Menu 2</span></li>
+            </ul>
+          </li>
+        </ul>
       </div>
       <div class="w-full">
         <!-- This ithe area where pages are rendered -->
@@ -31,15 +43,33 @@
   </div>
 </template>
 <script setup>
-import { Bars3Icon, XMarkIcon } from "@heroicons/vue/24/outline";
-import { onMounted, onUnmounted } from "vue";
+import {
+  Bars3Icon,
+  XMarkIcon,
+  ChevronDownIcon,
+} from "@heroicons/vue/24/outline";
+import { onMounted, onUnmounted, ref } from "vue";
 const isCollapse = ref(false);
 
+const windowWidthRef = ref(0);
 onMounted(() => {
-  console.log("I am on Mounted");
+  window.addEventListener("resize", handleResize);
+  windowWidthRef.value = window.innerWidth;
 });
 
 onUnmounted(() => {
-  console.log("I am on Unmounted");
+  window.removeEventListener("resize", handleResize);
+});
+
+const handleResize = () => {
+  windowWidthRef.value = window.innerWidth;
+};
+
+watch(windowWidthRef, (newValue, oldValue) => {
+  if (newValue < 1280) {
+    isCollapse.value = true;
+  } else {
+    isCollapse.value = false;
+  }
 });
 </script>
