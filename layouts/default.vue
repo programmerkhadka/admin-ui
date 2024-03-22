e624d4177c9903906826f4dc7fbc91bcaad37d8e
programmerkhadka <rituparnak@claritytech.io>
2024-03-22 13:58:42 +0530

    feat: Sidebar conctruction level 2

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="min-h-screen bg-gray-50">
     <div
-      class="w-full bg-white topbar min-h-[70px] border-b-2 py-2 px-4 grid grid-cols-2 items-center justify-between"
+      class="w-full bg-white topbar min-h-[70px] border-b-2 py-2 px-4 grid grid-cols-2 items-center justify-between top-0 sticky z-10"
     >
       <div class="flex place-items-center">
         <div
@@ -16,15 +16,13 @@
         <div class="w-10 h-10 border border-red-400"></div>
         <div class="w-10 h-10 border border-red-400"></div>
       </div>
-
-      <!--  <XMarkIcon class="w-8 h-8" /> -->
     </div>
     <div class="relative flex">
       <div
-        class="min-h-screen bg-white border-r-2 sidebra transition-[width] duration-150 ease-in-out absolute left-0 xl:static overflow-hidden"
+        class="bg-white border-r-2 sidebra transition-[width] duration-150 ease-in-out absolute left-0 xl:sticky xl:left-0 overflow-hidden"
         :class="isCollapse ? 'w-0' : ' w-96'"
       >
-        <ul class="flex flex-col">
+        <ul class="flex flex-col h-[90vh] overflow-y-scroll">
           <li v-for="(items, index) in menuItems">
             <a
               href="#"
@@ -185,6 +183,46 @@ const menuItems = [
             icon: "",
             title: "- Activity",
             children: [
+              {
+                text: "--- Social Media",
+                slug: "social-media",
+                href: "",
+                icon: "",
+                title: "--- Social Media",
+                children: [],
+              },
+              {
+                text: "--- Security",
+                slug: "security",
+                href: "",
+                icon: "",
+                title: "- Security",
+                children: [],
+              },
+              {
+                text: "--- Project",
+                slug: "project",
+                href: "",
+                icon: "",
+                title: "--- Project",
+                children: [],
+              },
+              {
+                text: "--- Hardware",
+                slug: "hardware",
+                href: "",
+                icon: "",
+                title: "--- Hardware",
+                children: [],
+              },
+              {
+                text: "--- Activity",
+                slug: "activity",
+                href: "",
+                icon: "",
+                title: "--- Activity",
+                children: [],
+              },
               {
                 text: "--- Social Media",
                 slug: "social-media",
